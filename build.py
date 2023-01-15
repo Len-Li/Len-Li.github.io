e70abd31771b2e717495970bfa3099692a37d2c6
Leheng Li <40287745+Len-Li@users.noreply.github.com>
2023-01-15 04:06:15 +0000

    update

--- build.py
+++ build.py
@@ -74,7 +74,7 @@ def get_author_dict():
         'Sebastian Nowozin': 'http://www.nowozin.net/sebastian/',
         }
 
-def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Michael Niemeyer', add_links=True):
+def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Leheng Li', add_links=True):
     links = get_author_dict() if add_links else {}
     s = ""
     for p in persons:
